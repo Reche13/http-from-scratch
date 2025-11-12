97e6a035b0cd0a1c49e3cab46ff2ee15aa195b57
Reche Soares <rechesoares13@gmail.com>
2025-11-12 11:57:30 +0530

    add graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type Server struct {
@@ -28,10 +31,18 @@ func (s *Server) ListenAndServe() error {
 
 	log.Printf("listening on %s", s.Addr)
 
+	go s.handleShutdownSignals()
+
 	for {
 		conn, err := s.ln.Accept()
 		if err != nil {
-			return fmt.Errorf("connection accepting error: %w", err)
+			select {
+			case <-s.done:
+				return nil
+			default:
+				log.Printf("connection accept error: %v", err)
+				continue
+			}
 		}
 
 		go s.handleConn(conn)
@@ -56,5 +67,20 @@ func (s *Server) handleConn(conn net.Conn) {
 	"Hello from http server\n"
 
 	conn.Write([]byte(resp))
+}
 
+func (s *Server) Close() {
+	close(s.done)
+	if s.ln != nil {
+		s.ln.Close()
+	}
+}
+
+func (s *Server) handleShutdownSignals() {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	log.Printf("shutting down...")
+	s.Close()
 }
