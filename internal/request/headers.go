79027eca98eea16bfc62c21b4cd008170fdec1b8
Reche Soares <rechesoares13@gmail.com>
2025-11-15 10:07:08 +0530

    add header parser

--- internal/request/headers.go
+++ internal/request/headers.go
@@ -1,6 +1,10 @@
 package request
 
-import "strings"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
 type Headers struct {
 	headers map[string]string
@@ -12,10 +16,60 @@ func NewHeaders() *Headers {
 	}
 }
 
+var ERROR_MALFORMED_FIELD_LINE = fmt.Errorf("malformed field-line")
+var ERROR_MALFORMED_FIELD_NAME = fmt.Errorf("malformed field-name")
+
 func (h *Headers) Set(name, value string) {
 	h.headers[strings.ToLower(name)] = value
 }
 
 func (h *Headers) Get(name string) string {
 	return h.headers[strings.ToLower(name)]
+}
+
+func parseHeader(data []byte) (string, string, error) {
+	parts := bytes.SplitN(data, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", ERROR_MALFORMED_FIELD_LINE
+	}
+
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", ERROR_MALFORMED_FIELD_NAME
+	}
+
+	return string(name), string(value), nil
+}
+
+
+func (h *Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+
+	for {
+		idx := bytes.Index(data, []byte(SEPARATOR))
+
+		if idx == -1 {
+			break
+		}
+
+		if idx == 0 {
+			done = true
+			read += len(SEPARATOR)
+			break
+		}
+
+		name, value, err := parseHeader(data[read:read + idx])
+		if err != nil {
+			return 0, false, err
+		}
+
+		idx += read + len(SEPARATOR)
+		h.Set(name, value)
+	}
+
+	return read, done, nil
+
 }
